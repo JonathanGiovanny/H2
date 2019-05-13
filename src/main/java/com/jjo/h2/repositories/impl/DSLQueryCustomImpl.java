c3f9099b4290e91e9072711a665d1819496654f7
Jonathan <jongiocam@gmail.com>
2019-05-13 14:30:08 -0500

    Remove Override notation from methods

--- src/main/java/com/jjo/h2/repositories/impl/DSLQueryCustomImpl.java
+++ src/main/java/com/jjo/h2/repositories/impl/DSLQueryCustomImpl.java
@@ -10,42 +10,34 @@ import com.querydsl.core.types.Predicate;
 
 public abstract class DSLQueryCustomImpl {
 
-  @Override
   public Optional<H> findOne(Predicate predicate) {
     return null;
   }
 
-  @Override
   public Iterable<H> findAll(Predicate predicate) {
     return null;
   }
 
-  @Override
   public Iterable<H> findAll(Predicate predicate, Sort sort) {
     return null;
   }
 
-  @Override
   public Iterable<H> findAll(Predicate predicate, OrderSpecifier<?>... orders) {
     return null;
   }
 
-  @Override
   public Iterable<H> findAll(OrderSpecifier<?>... orders) {
     return null;
   }
 
-  @Override
   public Page<H> findAll(Predicate predicate, Pageable pageable) {
     return null;
   }
 
-  @Override
   public long count(Predicate predicate) {
     return 0;
   }
 
-  @Override
   public boolean exists(Predicate predicate) {
     return false;
   }
