181e25453040669e829f31fe36e0a096e769019d
Jonathan <jongiocam@gmail.com>
2019-07-31 18:06:57 -0500

    Removed unused utility

--- src/main/java/com/jjo/h2/utils/Utils.java
+++ src/main/java/com/jjo/h2/utils/Utils.java
@@ -3,7 +3,6 @@ package com.jjo.h2.utils;
 import java.util.Collection;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.function.Predicate;
 import java.util.stream.Stream;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -11,20 +10,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Utils {
 
-  public static final Predicate<Object> IS_NUMBER = n -> n instanceof Long || n instanceof Integer;
-
   /**
    * Validates whenever a value is null or empty for a String
    */
   public static String isNotNullOr(String dto, String entity) {
     return Optional.ofNullable(dto).filter(s -> !s.isEmpty()).orElse(entity);
-  }
-
-  /**
-   * Validates whenever a value is null
-   */
-  public static <T extends Object> T isNotNullOr(T dto, T entity) {
-    return Optional.ofNullable(dto).orElse(entity);
   }
 
   /**
